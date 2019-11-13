9bdeb9b227a28f989b4f3a0b31e28aa0e69541b2
jinam@xiaoma.cn <jinam@xiaoma.cn>
2019-11-13 17:47:41 +0800

    fix

--- src/child.tsx
+++ src/child.tsx
@@ -1,23 +1,21 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import {TestContext} from "./index"
 
-interface Child {
-    // callback: (name: string) => void
-}
 
-function Child(props: Child) {
+function Child() {
     const state: any = useContext(TestContext);
+    const [num, setNum] = useState(0)
     return (
         <div>
             <button
                 onClick={() => {
-                    // props.callback("子组件名字")
                     state.childChangeName("子组件名字")
                 }}
             >
                 子组件
             </button>
             {state.name}
+            <p>子组件自身数字：{num}</p>
         </div>
     )
 }
